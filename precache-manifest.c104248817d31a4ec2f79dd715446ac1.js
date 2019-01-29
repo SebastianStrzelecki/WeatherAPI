1e9a059280a5b798521639595c346ef58c9e6092
Sebastian Strzelecki <sstrzelecki20@gmail.com>
2019-01-29 15:51:20 +0100

    Updates

---
+++ precache-manifest.c104248817d31a4ec2f79dd715446ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "342438937c11f168ade0",
+    "url": "/nazwa/static/js/runtime~main.34243893.js"
+  },
+  {
+    "revision": "087514f73c796fb63c45",
+    "url": "/nazwa/static/js/main.087514f7.chunk.js"
+  },
+  {
+    "revision": "a0e9a2fa3245b3159d7b",
+    "url": "/nazwa/static/js/1.a0e9a2fa.chunk.js"
+  },
+  {
+    "revision": "087514f73c796fb63c45",
+    "url": "/nazwa/static/css/main.9d7c16b3.chunk.css"
+  },
+  {
+    "revision": "a0e9a2fa3245b3159d7b",
+    "url": "/nazwa/static/css/1.15202450.chunk.css"
+  },
+  {
+    "revision": "e96fe6a1febdec473ba52b3bd4525b9e",
+    "url": "/nazwa/index.html"
+  }
+];
